2583a95d2e5a3e898d88a71740026e8f94556e20
Vladislav Pochernin <vspochernin@gmail.com>
2025-11-04 00:20:07 +0300

    spring boot initial commit

---
+++ src/main/java/ru/vspochernin/gigalearn/GigalearnApplication.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.gigalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigalearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigalearnApplication.class, args);
+	}
+
+}
